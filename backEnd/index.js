10fea75dbf6f8c60a7bb38d60eb31135123ce455
Som3a <som3a11202@gmail.com>
2024-11-01 23:52:49 +0200

    add vercel

--- backEnd/index.js
+++ backEnd/index.js
@@ -19,7 +19,7 @@ import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const port = 4000;
+const port = 4000 || process.env.PORT;
 
 // middlewares
 app.use(express.json());
